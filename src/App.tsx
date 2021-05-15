7d79c9f5391ca2752cebf21bae3e14bf93975352
Andrew Archer <andrew.m.archer@gmail.com>
2021-05-15 12:44:23 -0700

    add web-animations-js polyfill

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'web-animations-js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import SharedElementContextProvider from './components/SharedElementContextProvider';
